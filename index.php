aa644770942e4e3223747201ee89353fc0623a4a
65160212 <65160212@go.buu.ac.th>
2023-12-21 01:38:21 +0700

    65160212

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
-<html>
-    <body>
-        <h1>This is PHP<?php echo 'Hello World!'; ?></h1>
-        <div>
-            <p>1</p>
-            <?php
-            $var_a = 1;
-            $var_a = "1";
-            print_r($var_a);
-            var_dump($var_a);
-            echo $var_a + 2;
-            {
-                echo "<h1>my h1</h1>"
-            ?>
-            <p>2</p>
-            <?php
-            }
-            ?>
-            <p>3</p>
-        </div>
-    </body>
-</html>
+<!DOCTYPE html>
+<htmcl lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <p></p>
+    </center>
+</body>
+</htmcl>
