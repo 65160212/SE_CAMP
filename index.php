e0c09d4e9c74880745842862c5f22296cc033437
65160212 <65160212@go.buu.ac.th>
2023-12-24 22:15:18 +0700

    65160212

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
 </body>
 </html>
