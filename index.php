aa1a2ec930f593b4680763497ce5eb38cf256988
65160212 <65160212@go.buu.ac.th>
2023-12-26 21:35:07 +0700

    65160212

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="mystyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hedvig+Letters+Serif:opsz@12..24&family=Playfair+Display:ital,wght@1,400;1,600&family=Rubik+Bubbles&family=Sriracha&display=swap" rel="stylesheet">
 </head>
 <body>
 <!-- localhost/SE_CAMP/index.php -->
