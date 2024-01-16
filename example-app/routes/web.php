dcdfa2adb9708cc96c52eb8239e2ac2b970f687a
65160212 <65160212@go.buu.ac.th>
2024-01-16 20:43:56 +0700

    65160212

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,18 +1,23 @@
 <?php
-
+use App\Http\Controllers\MyControllers;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+Route::get('/my-controllers', [MyControllers::class, 'index']);
+Route::get('/my-controllers2','App\Http\Controllers\MyControllers@index');
+Route::resource('/my-controllers4',MyControllers::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-route', function () {
+    $data = ['var_a' => 'Hello World'];
+    $data['var_b'] ="Laraval";
+    return view('myfolder.mypage',$data);
+});
+use Illuminate\Http\Request;
+
+Route::post('/my-route', function (Request $request) {
+    $data['multiplier'] = $request->input('multiplier');
+    return view('myroute', $data);
+});
+
+Route::grt('/my-controller', [MyContrroller::class, 'index']);
